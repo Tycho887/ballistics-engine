b5f96f6eff6dde128b47d069cdc22d6dd7c5616e
michael <michaelrj03@gmail.com>
2026-02-02 21:00:42 +0100

    working implementing a basic demo

---
+++ engine/src/main.rs
@@ -0,0 +1,61 @@
+use nalgebra as na;
+
+struct Projectile {
+    mass: f32,
+    effective_area: f32,
+    position: na::Vector3<f32>,
+    velocity: na::Vector3<f32>,
+    drag_coefficient: f32,
+}
+
+fn atmospheric_density(_altitude: f32) -> f32 {
+    // Simplified model: constant density
+    return 1.225; // kg/m^3 at sea level
+}
+
+fn force_gravity() -> na::Vector3<f32> {
+    return na::Vector3::new(0.0, -9.81, 0.0)
+}
+
+fn drag_force(projectile: &Projectile) -> na::Vector3<f32> {
+
+    let velocity = projectile.velocity;
+    let speed = velocity.norm();
+    let _altitude = projectile.position.y;
+    let density = atmospheric_density(_altitude);
+    let drag_coefficient = projectile.drag_coefficient;
+    let area = projectile.effective_area;
+    return -0.5 * density * speed * drag_coefficient * area * velocity;
+}
+
+fn update_projectile(projectile: &mut Projectile, time_step: f32) {
+    //Update the projectile's position and velocity based on forces acting on it.
+    let gravity = force_gravity() * projectile.mass;
+    let drag = drag_force(&projectile);
+    let total_force = gravity + drag;
+
+    let acceleration = total_force / projectile.mass; // Assuming mass = 1 for simplicity
+    projectile.velocity += acceleration * time_step;
+    projectile.position += projectile.velocity * time_step;
+}
+
+fn main() {
+    let mut ballistic_rocket = Projectile {
+        mass: 100f32,
+        effective_area: 1f32,
+        position: na::Vector3::new(0.0f32, 0.0f32, 0.0f32),
+        velocity: na::Vector3::new(100.0f32, 100.0f32, 0.0f32),
+        drag_coefficient: 0.1f32,
+    };
+
+    let time_step: f32 = 0.1f32;
+
+    let mut seconds: f32 = 0f32;
+    while ballistic_rocket.position.y >= 0.0 {
+        update_projectile(&mut ballistic_rocket, time_step);
+        seconds += time_step;
+        println!("Time: {}s, Position: {:?}, Velocity: {:?}", seconds, ballistic_rocket.position, ballistic_rocket.velocity);
+    }
+    println!("Projectile has hit the ground at time: {}s", seconds);
+
+}
