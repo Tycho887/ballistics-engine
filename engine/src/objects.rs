453cfb751c23253dc286b43f785444d6d91f3bc8
michael <michaelrj03@gmail.com>
2026-05-15 19:48:20 +0200

    working on implementing nyx

--- engine/src/objects.rs
+++
@@ -1,9 +0,0 @@
-use nalgebra as na;
-
-pub struct Projectile {
-    pub mass: f32,
-    pub effective_area: f32,
-    pub position: na::Vector3<f32>,
-    pub velocity: na::Vector3<f32>,
-    pub drag_coefficient: f32,
-}
